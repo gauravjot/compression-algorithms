ea07d595e643a78f80bf9f8466b8855a655476b8
Gauravjot Garaya <gauravjot@outlook.com>
2024-02-12 08:50:50 -0800

    commit

--- python/lzma/lzma_unittest.py
+++ python/lzma/lzma_unittest.py
@@ -1,81 +1,80 @@
 from lzma import compress, decompress
 import time
 import unittest
+import os
 
 
 class TestLZMA(unittest.TestCase):
 
     def test_binary_numeric(self):
-        test_string = "122122112211122112221122112111212221222111"
-        result, compression_ratio, time_delta = perform_test(test_string)
-        if result:
-            print_stats(self.id(), test_string,
-                        compression_ratio, time_delta)
-        self.assertTrue(result, f"\n\tTest failed for string: \n{test_string}")
+        s = "122122112211122112221122112111212221222111"
+        perform_test(s, self)
 
     def test_alphabet_string1(self):
-        test_string = "ABABABAABAB"
-        result, compression_ratio, time_delta = perform_test(test_string)
-        if result:
-            print_stats(self.id(), test_string,
-                        compression_ratio, time_delta)
-        self.assertTrue(result, f"\n\tTest failed for string: \n{test_string}")
+        s = "ABABABAABAB"
+        perform_test(s, self)
 
     def test_alphabet_string2(self):
-        test_string = "ASDCNNCASSSMCCAMSDMMDSASDASDCNNCASSSMCCAMSDMMDSASD"
-        result, compression_ratio, time_delta = perform_test(test_string)
-        if result:
-            print_stats(self.id(), test_string,
-                        compression_ratio, time_delta)
-        self.assertTrue(result, f"\n\tTest failed for string: \n{test_string}")
+        s = "ASDCNNCASSSMCCAMSDMMDSASDASDCNNCASSSMCCAMSDMMDSASD"
+        perform_test(s, self)
 
     def test_alphabet_string3(self):
-        test_string = "ASDCNNCASSSMCCAMSDMMDSASDASDCNNCASSSMCCAMSDMMDSASDASDCNNCASSSMCCAMSDMMDSASD"
-        result, compression_ratio, time_delta = perform_test(test_string)
-        if result:
-            print_stats(self.id(), test_string,
-                        compression_ratio, time_delta)
-        self.assertTrue(result, f"\n\tTest failed for string: \n{test_string}")
+        s = "ASDCNNCASSSMCCAMSDMMDSASDASDCNNCASSSMCCAMSDMMDSASDASDCNNCASSSMCCAMSDMMDSASD"
+        perform_test(s, self)
 
     def test_alphanumeric_string1(self):
-        test_string = "ASDMC1231ASDCAASD2313DASCASD23235488ASDCASDMC1231ASDCAASD2313DASCASD23235488ASDCASDMC1231ASDCAASD2313DASCASD23235488ASDC"
-        result, compression_ratio, time_delta = perform_test(test_string)
-        if result:
-            print_stats(self.id(), test_string,
-                        compression_ratio, time_delta)
-        self.assertTrue(result, f"\n\tTest failed for string: \n{test_string}")
+        s = "ASDMC1231ASDCAASD2313DASCASD23235488ASDCASDMC1231ASDCAASD2313DASCASD23235488ASDCASDMC1231ASDCAASD2313DASCASD23235488ASDC"
+        perform_test(s, self)
 
     def test_alphanumeric_string2(self):
-        test_string = "ASDMC1231ASDCAASD2313DASCASD23235488ASDCASDMC1231ASDCAASD2313DASCASD23235488ASDC"
-        result, compression_ratio, time_delta = perform_test(test_string)
-        if result:
-            print_stats(self.id(), test_string,
-                        compression_ratio, time_delta)
-        self.assertTrue(result, f"\n\tTest failed for string: \n{test_string}")
+        s = "ASDMC1231ASDCAASD2313DASCASD23235488ASDCASDMC1231ASDCAASD2313DASCASD23235488ASDC"
+        perform_test(s, self)
 
     def test_alphanumeric_with_space_string1(self):
-        test_string = "ASDM C1231 ASDCA ASD23 13DASCASD232354 88ASDCASDMC1231AS DCAASD  2313DASC ASD2323548 8ASDC    ASDM C1231ASDCAA  SD2313D ASCASD23235488ASDC "
-        result, compression_ratio, time_delta = perform_test(test_string)
-        if result:
-            print_stats(self.id(), test_string,
-                        compression_ratio, time_delta)
-        self.assertTrue(result, f"\n\tTest failed for string: \n{test_string}")
+        s = "ASDM C1231 ASDCA ASD23 13DASCASD232354 88ASDCASDMC1231AS DCAASD  2313DASC ASD2323548 8ASDC    ASDM C1231ASDCAA  SD2313D ASCASD23235488ASDC "
+        perform_test(s, self)
 
     def test_alphanumeric_with_space_string2(self):
-        test_string = " ASDMC1231  ASDCAASD231 3DASCASD2323 5488ASDCASDMC 1231ASDCAA SD2313DAS CASD2 3 2 3 5 488ASDC"
-        result, compression_ratio, time_delta = perform_test(test_string)
-        if result:
-            print_stats(self.id(), test_string,
-                        compression_ratio, time_delta)
-        self.assertTrue(result, f"\n\tTest failed for string: \n{test_string}")
+        s = " ASDMC1231  ASDCAASD231 3DASCASD2323 5488ASDCASDMC 1231ASDCAA SD2313DAS CASD2 3 2 3 5 488ASDC"
+        perform_test(s, self)
 
     def test_complex1(self):
-        test_string = "ASCII, stands for American Standard Code for Information Interchange. It is a 7-bit character code where each individual bit represents a unique character. This page shows the extended ASCII table which is based on the Windows-1252 character set which is an 8 bit ASCII table with 256 characters and symbols."
-        result, compression_ratio, time_delta = perform_test(test_string)
-        if result:
-            print_stats(self.id(), test_string,
-                        compression_ratio, time_delta)
-        self.assertTrue(result, f"\n\tTest failed for string: \n{test_string}")
+        s = "ASCII, stands for American Standard Code for Information Interchange. It is a 7-bit character code where each individual bit represents a unique character. This page shows the extended ASCII table which is based on the Windows-1252 character set which is an 8 bit ASCII table with 256 characters and symbols."
+        perform_test(s, self)
+
+    def test_complex2(self):
+        s = "In the heart of the sprawling city, nestled between skyscrapers and bustling streets, lay a forgotten park. Its once vibrant foliage now wilted with neglect, whispered secrets of days gone by. Hannah found solace in its overgrown paths, weaving through memories as tangled as the ivy that adorned its crumbling walls. Each step brought echoes of laughter and tears, mingling with the rustle of fallen leaves. Here, beneath the watchful gaze of weathered statues, she found sanctuary from the chaos of the world, a place where time stood still and dreams took flight on the wings of forgotten whispers."
+        perform_test(s, self)
+
+    def test_complex3(self):
+        s = "In the neon-lit streets of Sector 7B, where the echoes of a thousand footsteps merged with the hum of machinery, stood an old diner, its sign flickering like a heartbeat on the edge of oblivion. The air crackled with the energy of a city that never slept, each moment measured in the pulse of digital displays and the blur of passing cars. Behind the counter, Sam poured cups of steaming coffee, the aroma mingling with the sharp tang of grease and dreams deferred. The diner was a haven for lost souls and weary travelers, where stories unfolded like chapters in a book, each booth a sanctuary from the relentless march of time. Amidst the clatter of dishes and the drone of conversation, Sam found solace in the rhythm of the night, where numbers danced like fireflies in the darkness, guiding lost souls home."
+        perform_test(s, self)
+
+    def test_complex4(self):
+        s = "In the shadow of Mount Everest, where the air was thin and the temperature plunged below freezing, a team of climbers embarked on a journey to conquer the world's tallest peak. With a base camp situated at an altitude of 5,000 meters, they braved avalanches and crevasses in their quest to reach the summit, their determination unwavering despite the odds."
+        perform_test(s, self)
+
+    def test_complex5(self):
+        s = "The drift velocity then determines the electric current density and its relationship to E and is independent of the collisions. Drude calculated the average drift velocity from p = −eEτ where p is the average momentum, −e is the charge of the electron and τ is the average time between the collisions. Since both the momentum and the current density are proportional to the drift velocity, the current density becomes proportional to the applied electric field; this leads to Ohm's law."
+        perform_test(s, self)
+
+    def test_large_file1(self):
+        test_file("test_files/ai_generated1.txt", self)
+
+    def test_large_file2(self):
+        test_file("test_files/ohms_law.txt", self)
+
+    def test_large_file3(self):
+        test_file("test_files/psychology.txt", self)
+
+    def test_large_file4(self):
+        test_file("test_files/bigfile.txt", self)
+
+    # def test_large_file5(self):
+    #     test_file("test_files/bigfile2.txt", self)
+
+    # def test_large_file6(self):
+    #     test_file("test_files/bigfile10.txt", self)
 
 
 """
@@ -83,15 +82,20 @@ class TestLZMA(unittest.TestCase):
 """
 
 
-def compression_ratio(original: str, compressed: str) -> float:
-    return len(compressed) / len(original)
-
-
-def perform_test(test_string: str):
+def perform_test(test_string: str, self):
     # track time
     start_time = time.time()
     compressed = compress(test_string)
     decompressed = decompress(compressed)
+    result = test_string == decompressed
+
+    if result:
+        print_stats(self.id(),
+                    test_string,
+                    compression_ratio(test_string, compressed),
+                    time.time() - start_time
+                    )
+    self.assertTrue(result)
 
     return (
         test_string == decompressed,
@@ -100,11 +104,20 @@ def perform_test(test_string: str):
     )
 
 
+def test_file(file_path: str, self):
+    with open(os.path.join(os.path.dirname(__file__), file_path), "r") as f:
+        test_string = f.read()
+        perform_test(test_string, self)
+
+
+def compression_ratio(original: str, compressed: str) -> float:
+    return len(compressed) / len(original)
+
+
 def print_stats(test_name: str, test_string: str, compression_ratio: float, time_delta: float):
     print("\n======================================================================")
     print(f"Test passed : {test_name}")
     print("----------------------------------------------------------------------\n")
-    print(f"String\t\t\t: {test_string}")
     print(f"Compression Ratio\t: {compression_ratio}")
     print("Time\t\t\t: {:.10f}s\n".format(time_delta))
 
